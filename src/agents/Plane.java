af9b51de81d6c0d0cb5184acd1ed41a38edd15cf
Fábio <Azevedo>
2019-10-15 18:04:38 +0100

    Started agent communication

--- src/agents/Plane.java
+++ src/agents/Plane.java
@@ -1,23 +1,40 @@
 package agents;
+import jade.core.AID;
+import jade.domain.AMSService;
+import jade.domain.FIPAAgentManagement.AMSAgentDescription;
+import jade.domain.FIPAAgentManagement.SearchConstraints;
 import jade.core.Agent;
 import jade.core.behaviours.*;
+import jade.lang.acl.ACLMessage;
 
 @SuppressWarnings("serial")
 public class Plane extends Agent 
 {       
 	int flightHours;
 	int fuelLeft;
+	AMSAgentDescription [] agents = null;
+	AID myID;
+	ACLMessage msg = null;
 	
+	@SuppressWarnings("deprecation")
 	protected void setup() 
     {
     	Object[] args = getArguments();
-    	
      	String s = (String) args[0];
-     	
      	String[] split = s.split(" ");
 
     	flightHours = Integer.parseInt(split[0]);
     	fuelLeft = Integer.parseInt(split[1]);
+    	
+    	try {
+    		SearchConstraints c = new SearchConstraints();
+    		c.setMaxResults(new Long(-1));
+    		agents = AMSService.search(this, new AMSAgentDescription (), c);
+    	} catch (Exception e) {
+    		
+    	}
+    	
+    	myID = getAID();
     	
         addBehaviour( 
 
@@ -27,12 +44,42 @@ public class Plane extends Agent
                 
                 public void action() 
                 {	
-                    System.out.println( "Hello World! My name is " + getLocalName() + " I have " + fuelLeft +" fuel left and can fligh more " +  flightHours);
-                    n++;
+                	String name = getLocalName();
+
+                	if(name.equals("JESUS2") && msg == null) {
+                		
+	                    n++;
+	                    
+	                    msg = new ACLMessage(ACLMessage.REFUSE);
+	                	msg.setContent("Hi! Wanna date?");
+	                	
+	                	for (int i =  0; i < agents.length; i++) {
+	                		//if (!agents[i].getName().getLocalName().contentEquals(name))
+	                		msg.addReceiver(agents[i].getName());
+	                	}
+	                	
+	                	send(msg);
+	                	
+                	} else {
+                		msg = receive();
+                		            		
+                		if(msg != null) {
+                			System.out.println("Im " + getLocalName() + " and I recieve: " + msg.getContent() + " from " + msg.getSender());
+                			if(!name.equals("JESUS2")) {
+                			
+                				ACLMessage reply = msg.createReply();
+                				reply.setPerformative(ACLMessage.INFORM);
+                				reply.setContent("YESSSSSSS!!!!");
+                				send(reply);
+                			}
+                		}
+                		block();
+                	}
+	                	
                 }
         
                 public boolean done() {  
-                	return n>=3;  
+                	return n>=2;  
                 }
             }
         );
